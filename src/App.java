a329217b1b3f19f2a21a46b265b139352fac5ed6
Tuanphong166 <mobiistar2st@gmail.com>
2021-12-15 14:35:00 +0700

    70%

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("San pham : Tao  ,Ngay nhap kho : 11-12-2021");
+     
     }
 }
